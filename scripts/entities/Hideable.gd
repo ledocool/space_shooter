971141feb6e50f732f223cc39f995f7dc420fa77
ledo <ledocool>
2020-03-08 20:40:09 +0700

    Lastroom wire & door

--- scripts/entities/Hideable.gd
+++ scripts/entities/Hideable.gd
@@ -1,4 +1,3 @@
-tool
 extends Node2D
 
 export var showSpeed = 0.5
@@ -6,29 +5,36 @@ export var hideSpeed = 0.3
 var visibility = 1.0
 var hide = false
 
-func _ready():
-	if(Engine.is_editor_hint()):
-		set_process(false)
-		setVisibility(0.3)
-		
+#func _ready():
+#	if(Engine.is_editor_hint()):
+#		set_process(false)
+#		setVisibility(0.3)
+#
 # warning-ignore:unsafe_method_access
-	if(!$VisibilityEnabler2D.is_on_screen()):
-		set_process(false)
+#	if(!$VisibilityEnabler2D.is_on_screen()):
+#		set_process(false)
+#		if(hide):
+#			visibility = 0.0
+#		else:
+#			visibility = 1.0
+#		setVisibility(visibility)
 
 func _process(delta):
+	if(hide == false && visibility >= 1.0
+	|| hide == true && visibility <= 0.0):
+		return
+		
 	if(hide == false && visibility < 1.0):
 		visibility += delta * showSpeed
 	elif(hide == true && visibility > 0.0):
 		visibility -= delta * hideSpeed
-		
+	
 	if(visibility < 0.0):
 		visibility = 0.0
 	elif(visibility > 1.0):
 		visibility = 1.0
 		
-	if(hide == true && visibility > 0.0
-		|| hide == false && visibility < 1.0):
-		setVisibility(visibility)
+	setVisibility(visibility)
 
 func Hide():
 	hide = true
@@ -43,3 +49,10 @@ func setVisibility(vis):
 		vis = 0.0
 	(material as ShaderMaterial).set_shader_param("visibility", vis)
 
+
+func _on_VisibilityEnabler2D_screen_exited():
+	if(hide):
+		visibility = 0.0
+	else:
+		visibility = 1.0
+	setVisibility(visibility)
