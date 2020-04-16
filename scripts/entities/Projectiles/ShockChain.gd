ded8b6a0ec9e8a2a9467f5e71ee0260404801cbe
ledo <ledocool>
2020-04-16 14:53:49 +0700

    Shock shoot

--- scripts/entities/Projectiles/ShockChain.gd
+++ scripts/entities/Projectiles/ShockChain.gd
@@ -3,6 +3,9 @@ extends Node2D
 var shockPlate = preload("res://scenes/entities/ConcreteEntities/Bullets/ShockPlate.tscn")
 var enitityCounter = 0
 
+export var entityOffset = 95
+export var entityMax = 15
+
 
 func SpawnAt(pos: Vector2, angle: float, _add_velocity: Vector2):
 	rotation = angle
@@ -14,7 +17,16 @@ func spawnChild(position: Vector2):
 	instance.connect("tree_exited", self, "_onTreeExiting")
 	instance.position = position
 	add_child(instance)
-	enitityCounter += 1
+
+
+func _ready():
+	var currentPos = 0
+	for i in range(0, entityMax):
+		currentPos = (i + 1)*entityOffset
+		var plate = Position2D.new()
+		plate.name = "Position2D" + String(i)
+		plate.position = Vector2(currentPos, 0)
+		add_child(plate)
 
 
 func _onTreeExiting():
@@ -24,12 +36,11 @@ func _onTreeExiting():
 
 
 func _on_Timer1_timeout():
-	spawnChild($Position2D1.position)
-
-
-func _on_Timer2_timeout():
-	spawnChild($Position2D2.position)
-
-
-func _on_Timer3_timeout():
-	spawnChild($Position2D3.position)
+	if(enitityCounter >= entityMax):
+		$Timers/Timer1.stop()
+		return
+	
+	var posName = "Position2D" + String(enitityCounter)
+	var pos = get_node(posName)
+	spawnChild(pos.position)
+	enitityCounter += 1
