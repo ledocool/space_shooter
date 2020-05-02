56aa9ab251f74a34a0b126cc80a86b82773cf9ed
ledo <ledo>
2020-05-02 11:59:40 +0700

    Custom build basic outline
    - custom shape building created basic outline
    - put turret shot on the background

--- scripts/entities/CustomShapeBuilding.gd
+++ scripts/entities/CustomShapeBuilding.gd
@@ -2,9 +2,93 @@ tool
 extends Node2D
 class_name CustomShapeBilding
 
+var dot = preload("res://resources/Effects/outline.png")
+var dotImage = dot.get_data()
+
+var poly:PoolVector2Array
+var img = Image.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var polygon = $ColliderBody/CollisionPolygon2D.polygon
-	$Walls.polygon = polygon
+	$Walls/Walls.polygon = polygon
+	poly = polygon
+	_createOutline(polygon)
+
+#func _draw():
+#	var first = true
+#	var lastNode = null
+#	var firstNode = null
+#
+#	for node in poly:
+#		draw_circle(node, 20, Color.red)
+#
+#		if (lastNode == null):
+#			firstNode = node
+#		else:
+#			draw_line(lastNode, node, Color.red, 20)
+#
+#		lastNode = node
+#
+#	if(firstNode != null):
+#		draw_line(firstNode, lastNode, Color.red, 20)
+		
+
+func _createOutline(shape: PoolVector2Array):
+	var margin = dotImage.get_size()
+	var rect = _findTextureDimensions(shape, dotImage.get_size())
+	img.create(rect.size.x, rect.size.y, false, dotImage.get_format())
+
+	var first = true
+	var lastNode = null
+	var firstNode = null
+
+	var dotSize = margin / 2;
+
+	for node in shape:
+		_addDot(rect.end - node)
+
+		if (lastNode == null):
+			firstNode = node
+		else:
+			_addDots(rect.end - lastNode, rect.end - node, dotSize)
+
+		lastNode = node
+		
+
+	if(firstNode != null):
+		_addDots(rect.end - firstNode, rect.end - lastNode, dotSize)
+
+	var texture = ImageTexture.new()
+	texture.create_from_image(img)
+	var result = $Sprite.set_texture(texture)
+	$Sprite.position = rect.end + (margin / 2)
+
+
+func _findTextureDimensions(shape: PoolVector2Array, margin: Vector2) -> Rect2:
+	var xSt = 0 
+	var ySt = 0
+	var xEd = 0
+	var yEd = 0
+	var first = true
+	
+	for node in shape:
+		if(node.x > xEd || first): xEd = node.x
+		if(node.x < xSt || first): xSt = node.x
+		if(node.y > yEd || first): yEd = node.y
+		if(node.y < ySt || first): ySt = node.y
+		first = false
+	
+	return Rect2(floor(xSt - margin.x), floor(ySt - margin.y), ceil(xEd - xSt + margin.x), ceil(yEd - ySt + margin.y))
+
+func _addDot(to: Vector2):
+	img.blend_rect(dotImage, Rect2(Vector2.ZERO, dotImage.get_size()), to)
+
+
+func _addDots(from: Vector2, to: Vector2, dotSize: Vector2):
+	var movement = to - from
+	var dotAmount = ceil(movement.length() / dotSize.x) #20 is dotsize
+	for i in range(1, dotAmount):
+		var offset = from + i * movement / dotAmount
+		_addDot(offset)
 
