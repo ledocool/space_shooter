1dec354b6ffa3d9aee5aeba014daeef998cdeb96
ledo <ledocool1@gmail.com>
2020-08-03 00:13:56 +0700

    make looping soind not annoying

--- scripts/sound/LoopableSfx.gd
+++ scripts/sound/LoopableSfx.gd
@@ -1,30 +1,23 @@
 extends Node2D
 
-var minVal = -80
-var maxVal = 0
-var duration = 0.1
+var MustPlay: bool = false
 
 func Start():
-	$AudioStreamPlayer2D.volume_db = 0
+	MustPlay = true
+	$AudioStreamPlayer2D.volume_db = -15
+	$Timer.stop()
 	if(!$AudioStreamPlayer2D.playing):
 		$AudioStreamPlayer2D.play()
 	
 func Stop():
-	$AudioStreamPlayer2D.stop()
-	$AudioStreamPlayer2D2.play(4.999)
+	MustPlay = false
+	$Timer.start()
+	$AudioStreamPlayer2D2.play()
+	
 
 func _ready():
 	pass
 
-
-func _on_Tween_tween_completed(object, key):
-	if(object is AudioStreamPlayer2D):
-		object.stop()
-
-
-func _on_Tween_tween_step(object, key, elapsed, value):
-	print_debug(value)
-
-
-func _on_Tween_tween_started(object, key):
-	pass # Replace with function body.
+func _on_Timer_timeout():
+	if(!MustPlay):
+		$AudioStreamPlayer2D.stop()
