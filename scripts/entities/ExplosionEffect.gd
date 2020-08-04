3d1773439705db6515dabff34671f56227430130
ledo <ledocool1@gmail.com>
2020-08-04 21:47:57 +0700

    redo explosion animation

--- scripts/entities/ExplosionEffect.gd
+++ scripts/entities/ExplosionEffect.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+func _ready():
+	$Particles2D.emitting = true
+	$AnimatedSprite.play()
+
 func _on_AnimatedSprite_animation_finished():
 	($AnimatedSprite as AnimatedSprite).stop()
 	self.visible = false
