246ef51351e52aae6aff825e50b2a60e94814cde
ledo <ledocool1@gmail.com>
2020-09-06 18:40:49 +0700

    add cheat menu open logic

---
+++ scripts/ui/CheatMenuPin.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+var isGrabbed = false
+var isMouseOver = false
+export var detectPressDistance = 300
+onready var startPosition = self.position
+
+func _input(event):
+	if(is_visible_in_tree() == false):
+		return
+	
+	if (event is InputEventMouse):
+		var distance = (event.position - startPosition).length()
+		if(distance < detectPressDistance):
+			isMouseOver = true
+		else:
+			isMouseOver = false
+		
+	if (event is InputEventMouseButton && event.button_index == BUTTON_LEFT):
+		if(event.is_pressed() && isMouseOver):
+			isGrabbed = true
+		else:
+			isGrabbed = false
+			self.position = startPosition
+
+func _physics_process(_delta):
+	if(isGrabbed):
+		var mousePosition = get_global_mouse_position()
+		var speedVector = mousePosition - position
+# warning-ignore:return_value_discarded
+		move_and_slide(speedVector)
+
--- scripts/ui/EscapeMenu.gd
+++ scripts/ui/EscapeMenu.gd
@@ -4,6 +4,9 @@ signal save_game()
 signal load_game()
 signal options()
 
+func _ready():
+	_on_EscapeMenu_visibility_changed()
+
 func _unhandled_key_input(event):
 	if(event.is_action_pressed("ui_menu")):
 		self.visible = !self.visible
@@ -35,3 +38,14 @@ func _on_Exit_pressed():
 # warning-ignore:return_value_discarded
 	($"/root/LevelLoader" as LevelLoader).LoadLevelByName("res://scenes/interface/MainMenu.tscn", false)
 	visible = false
+
+
+func _on_EscapeMenu_visibility_changed():
+	var pin = $CheatMenuEnabler/Pin
+	pin.set_pickable(self.visible)
+	pin.set_physics_process(self.visible)
+	pin.set_process_input(self.visible)
+
+
+func _on_CheatMenuSwitch_body_entered(_body):
+	print("Cheat menu enabled!")
