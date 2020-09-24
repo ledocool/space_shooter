0f8298d99336debe7af9155e8a69d4e8a7580620
ledo <ledocool1@gmail.com>
2020-09-24 20:47:05 +0700

    spiral scripts

---
+++ scripts/levels/c1l2/SpiralEnterTrigger.gd
@@ -0,0 +1,45 @@
+extends Node
+
+var opened = false
+
+onready var generator = $"/root/Level/DynamicScenery/SpiralGenerator"
+onready var wires = get_tree().get_nodes_in_group("spiral_entrance_wire")
+onready var door = $"/root/Level/DynamicScenery/SpiralEntrance"
+
+func Save():
+	return {
+		"opened": opened
+	}
+
+
+func Load(data: Dictionary):
+	opened = data.opened
+
+
+func _ready():
+	if(!opened):
+		generator.Enable()
+	else:
+		generator.Disable()
+	
+	_switch(!opened)
+
+
+func _switch(isOn: bool):
+	if(door != null):
+		if(isOn):
+			door.call_deferred("Enable")
+		else:
+			door.call_deferred("Disable")
+	
+	if(wires != null):
+		for wire in wires:
+			if(isOn):
+				wire.Enable()
+			else:
+				wire.Disable()
+
+
+func _on_SpiralGenerator_wire_switch(isOn):
+	opened = !isOn
+	_switch(!opened)
---
+++ scripts/levels/c1l2/SpiralTrigger.gd
@@ -0,0 +1,76 @@
+extends Node
+
+var horizontalOpened = true
+var verticalOpened = false
+
+onready var generator = $"/root/Level/DynamicScenery/SpiralGenerator"
+onready var wires = get_tree().get_nodes_in_group("inspiral_wire")
+onready var horizontalDoors = get_tree().get_nodes_in_group("spiral_horizontal_doors")
+onready var verticalDoors = get_tree().get_nodes_in_group("spiral_vertical_doors")
+
+func Save():
+	return {
+		"horizontalOpened": horizontalOpened,
+		"verticalOpened": verticalOpened
+	}
+
+
+func Load(data: Dictionary):
+	horizontalOpened = data.horizontalOpened
+	verticalOpened = data.verticalOpened
+
+
+func _ready():
+	if(generatorOn()):
+		generator.Enable()
+	else:
+		generator.Disable()
+	
+	_switchHorizontal(!horizontalOpened)
+	_switchVertical(!verticalOpened)
+	_switchWires(generatorOn())
+
+
+func _switchHorizontal(isOn: bool):
+	if(horizontalDoors != null):
+		for door in horizontalDoors:
+			if(isOn):
+				door.call_deferred("Enable")
+			else:
+				door.call_deferred("Disable")
+
+
+func _switchVertical(isOn: bool):
+	if(verticalDoors != null):
+		for door in verticalDoors:
+			if(isOn):
+				door.call_deferred("Enable")
+			else:
+				door.call_deferred("Disable")
+
+
+func _switchWires(isOn: bool):
+	if(wires != null):
+		for wire in wires:
+			if(isOn):
+				wire.Enable()
+			else:
+				wire.Disable()
+
+
+func generatorOn():
+	return horizontalOpened == true && verticalOpened == false
+
+
+func _on_InspiralGenerator_wire_switch(isOn):
+	horizontalOpened = false
+	verticalOpened = true
+	_switchHorizontal(!horizontalOpened)
+	_switchVertical(!verticalOpened)
+	_switchWires(generatorOn())
+
+
+
+func _on_KeyPickup_picked_up(data):
+	horizontalOpened = true
+	_switchHorizontal(!horizontalOpened)
