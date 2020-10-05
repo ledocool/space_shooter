20dd7bd1f1950a521c43d73381243411bc4f4fa3
ledo <ledocool1@gmail.com>
2020-10-05 20:59:13 +0700

    explosion sound

--- scripts/entities/ExplosionEffect.gd
+++ scripts/entities/ExplosionEffect.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 func _ready():
-# warning-ignore:unsafe_property_access
-	$Particles2D.emitting = true
 # warning-ignore:unsafe_method_access
 	$AnimatedSprite.play()
+	
 
 func _on_AnimatedSprite_animation_finished():
 	($AnimatedSprite as AnimatedSprite).stop()
 	self.visible = false
 	self.queue_free()
+
+
+func _on_AudioStreamPlayer2D_finished():
+	pass # Replace with function body.
