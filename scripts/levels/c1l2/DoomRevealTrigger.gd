35f4d1bfe0071bae1ca12ce0a13366435b6e0136
ledo <ledocool1@gmail.com>
2020-10-11 20:02:47 +0700

    add doomguy secret

---
+++ scripts/levels/c1l2/DoomRevealTrigger.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+onready var secretAsteroids = get_tree().get_nodes_in_group("DoomChill")
+
+var found = false
+
+func _on_SecretRevealTrigger_body_entered(body):
+	if(body is PlayerShip):
+		if(!found):
+# warning-ignore:unsafe_property_access
+			$"/root/Level".playerSecretsFound += 1
+			found = true
+			
+		for top in secretAsteroids:
+			top.Hide();
+
+
+func _on_SecretRevealTrigger_body_exited(body):
+	if(body is PlayerShip):
+		for top in secretAsteroids:
+			top.Show();
